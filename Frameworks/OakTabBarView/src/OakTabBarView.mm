4802dfb2f4b48857139acf790c239824a7b31ace
Shane Becker <veganstraightedge@gmail.com>
2026-08-26 03:14:30 -0700

    Name the file URL pasteboard type as NSPasteboardTypeFileURL instead of the deprecated kUTTypeFileURL

--- Frameworks/OakTabBarView/src/OakTabBarView.mm
+++ Frameworks/OakTabBarView/src/OakTabBarView.mm
@@ -79,7 +79,7 @@ static NSString* const OakTabItemPasteboardType = @"com.macromates.TextMate.tabI
 
 - (NSArray*)writableTypesForPasteboard:(NSPasteboard*)aPasteboard
 {
-	return OakIsEmptyString(_path) ? @[ OakTabItemPasteboardType ] : @[ OakTabItemPasteboardType, (NSString*)kUTTypeFileURL ];
+	return OakIsEmptyString(_path) ? @[ OakTabItemPasteboardType ] : @[ OakTabItemPasteboardType, NSPasteboardTypeFileURL ];
 }
 
 - (NSPasteboardWritingOptions)writingOptionsForType:(NSString*)aType pasteboard:(NSPasteboard*)aPasteboard
@@ -89,7 +89,7 @@ static NSString* const OakTabItemPasteboardType = @"com.macromates.TextMate.tabI
 
 - (id)pasteboardPropertyListForType:(NSString*)aType
 {
-	if([aType isEqualToString:(NSString*)kUTTypeFileURL])
+	if([aType isEqualToString:NSPasteboardTypeFileURL])
 		return [NSURL fileURLWithPath:_path].absoluteString;
 
 	NSMutableDictionary* dict = [NSMutableDictionary dictionary];
