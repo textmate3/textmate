1f0a49d8e87d45f5e28f868e10174887d0b75522
Shane Becker <veganstraightedge@gmail.com>
2026-09-03 06:10:11 -0700

    Expose the pop up button's encoding codes and names as class methods, so another chooser can offer the same list

--- Frameworks/OakAppKit/src/OakEncodingPopUpButton.h
+++ Frameworks/OakAppKit/src/OakEncodingPopUpButton.h
@@ -1,3 +1,9 @@
 @interface OakEncodingPopUpButton : NSPopUpButton
 @property (nonatomic) NSString* encoding;
+
+// The encodings the user has chosen to see, in the order the pop up lists them:
+// each an iconv name and the name a person reads, so other choosers can offer
+// the same list without being a pop up.
++ (NSArray<NSString*>*)availableEncodingCodes;
++ (NSArray<NSString*>*)availableEncodingNames;
 @end
