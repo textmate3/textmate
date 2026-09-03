dcb58436ab3b3c02d33752d5f5f6650f9e357057
Shane Becker <veganstraightedge@gmail.com>
2026-09-03 05:55:30 -0700

    ApplicationInfo, the first Swift in the tree: version and copyright from Info.plist, with an Objective-C++ test through the generated header

---
+++ Frameworks/OakFoundation/tests/t_application_info.mm
@@ -0,0 +1,22 @@
+#import <OakFoundation/OakFoundation-Swift.h>
+
+// The first Swift in the tree, reached from Objective-C++ through the header the
+// Swift compiler generates. The test proves the round trip: a Swift class with an
+// Objective-C name, constructed and read from this side.
+
+void test_application_info_reads_the_dictionary ()
+{
+	OakApplicationInfo* info = [[OakApplicationInfo alloc] initWithInfoDictionary:@{
+		@"CFBundleShortVersionString": @"2.1.0",
+		@"NSHumanReadableCopyright":   @"© 2026",
+	}];
+	OAK_ASSERT_EQ(std::string(info.shortVersion.UTF8String), "2.1.0");
+	OAK_ASSERT_EQ(std::string(info.copyright.UTF8String), "© 2026");
+}
+
+void test_application_info_is_empty_without_keys ()
+{
+	OakApplicationInfo* info = [[OakApplicationInfo alloc] initWithInfoDictionary:@{}];
+	OAK_ASSERT_EQ(std::string(info.shortVersion.UTF8String), "");
+	OAK_ASSERT_EQ(std::string(info.copyright.UTF8String), "");
+}
