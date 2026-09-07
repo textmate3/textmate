7e581f9094c8c93284fde7fd9f62d47fc6cd46dc
Shane Becker <veganstraightedge@gmail.com>
2026-09-07 01:44:39 -0700

    The C and Mac preludes say include rather than import

--- Shared/PCH/prelude-mac.h
+++ Shared/PCH/prelude-mac.h
@@ -1,15 +1,15 @@
 #ifndef PRELUDE_MAC_H_X1SR1JB2
 #define PRELUDE_MAC_H_X1SR1JB2
 
-#import "prelude.c"
+#include "prelude.c"
 
-#import <CommonCrypto/CommonDigest.h>
-#import <CoreFoundation/CoreFoundation.h>
-#import <CoreServices/CoreServices.h>
-#import <Security/Security.h>
-#import <ServiceManagement/ServiceManagement.h>
+#include <CommonCrypto/CommonDigest.h>
+#include <CoreFoundation/CoreFoundation.h>
+#include <CoreServices/CoreServices.h>
+#include <Security/Security.h>
+#include <ServiceManagement/ServiceManagement.h>
 
-#import <os/activity.h>
-#import <os/log.h>
+#include <os/activity.h>
+#include <os/log.h>
 
 #endif /* end of include guard: PRELUDE_MAC_H_X1SR1JB2 */
